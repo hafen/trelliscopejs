f840a615b719c570dc899a86876f0cf38d2dbaaa
hafen <rhafen@gmail.com>
2018-09-27 00:01:22 -0700

    Spelling fixes

--- man-roxygen/param-split-aspect.R
+++ man-roxygen/param-split-aspect.R
@@ -1,1 +1,1 @@
-#' @param split_aspect list indicating aspect ratios of axes for a split layour.  Only applies to ggplot2 plot objects.
+#' @param split_aspect list indicating aspect ratios of axes for a split layout.  Only applies to ggplot2 plot objects.
